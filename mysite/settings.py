ea9f220fdc84f2abfd36bf39cb7ffc3d418a4508
Dicle Yalcin <dicle.yalcin2@gmail.com>
2018-04-25 15:59:32 -0500

    social

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'social.apps.django_app.default',
     'mysite',
     'rest_framework',
 ]
